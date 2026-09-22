61c6ea5cbf3f74109d507bdc841b246d05e1a223
Natalia Navarro <nataliasoledadnavarro@gmail.com>
2026-09-22 16:56:42 -0300

    feat: Configura proyecto base de Node.js y CLI para gestión de productos

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Inicialización del proyecto de Node.js");
+const [, , method, resource] = process.argv;
+
+if (!method || !resource) {
+  console.log("Uso: npm run start <METHOD> <RESOURCE>");
+} else {
+  console.log(`Método: ${method}`);
+  console.log(`Recurso: ${resource}`);
+}
